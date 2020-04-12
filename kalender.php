71602e9900e0945ddf09eacdb13fd1792001ff26
robergi1 <robergi1@tlu.ee>
2020-04-12 22:46:22 +0200

    kalender

--- kalender.php
+++ kalender.php
@@ -64,19 +64,18 @@ session_start();
               -moz-user-select: none;
               -ms-user-select: none;
               user-select: none;
-              left: 45.2vw;
-              top: -28vh;
+              left: 50.2vw;
+              top: -46vh;
           }
 
-          /* The actual popup */
           .popup .popuptext {
               visibility: hidden;
-              width: 35vw;
-              height: 39.2vw;
+              width: 25vw;
+              height: 20vw;
               background-color: #555;
               color: #fff;
               text-align: center;
-              line-height: 34vw;
+              line-height: 18vw;
               border-radius: 6px;
               padding: 8px 0;
               position: absolute;
