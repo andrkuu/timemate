01e394cd810148c3f62585e6d80afb80e68515b9
uWookies <martten@tlu.ee>
2020-04-12 18:23:13 +0300

    change logout to button

--- nav-bar.php
+++ nav-bar.php
@@ -11,9 +11,9 @@ $_SESSION["userLastName"] = "Noor";
     echo "<div id=\"topbar\"><img src=\"images/tlulogo.png\" id=\"tlu_logo\" alt=\"Tlu logo\"> 
     <div id='client_name'>".$_SESSION["userFirstName"]. " ".$_SESSION["userLastName"]."</div>
     <form method=\"POST\" action=\"". htmlspecialchars($_SERVER["PHP_SELF"])."\">
-        <div type=\"submit\" id=\"logout\" name=\"logout\">
+        <button type=\"submit\" id=\"logout\" name=\"logout\">
             <img id='logout_img' src=\"images/logout.png\" alt=\"logout\">
-        </div>           
+        </button>       
     </form>
     <link rel=\"stylesheet\" href=\"style.css\">
 </div>";
