3c02b656c59dd0b1071d172e56b6de55defa0aff
uWookies <martten@tlu.ee>
2020-06-10 11:12:22 +0300

    Burger Menu 1.0

--- nav-bar.php
+++ nav-bar.php
@@ -8,10 +8,39 @@
     }
     echo "<div id=\"topbar\"><img src=\"../images/tlulogo.png\" id=\"tlu_logo\" alt=\"Tlu logo\"> 
     <div id='client_name'><a href=\"../seaded\" id='client_link'>".$_SESSION["userFirstName"]. " ".$_SESSION["userLastName"]."</a></div>
-    <form method=\"POST\" action=\"". htmlspecialchars($_SERVER["PHP_SELF"])."\">
+    
+    <div id=\"myLinks\">
+    <a id=\"Links\" href=\"#news\">News</a>
+    <a id=\"Links\" href=\"#contact\">Contact</a>
+  </div>
+  <a href=\"javascript:void(0);\" id='img_container' onclick=\"myFunction()\">
+    <img id='dropdown_img' src=\"../images/dropdown.png\" alt=\"dropdown\">
+  </a>
+  
+  <script>
+    function myFunction() {
+      var x = document.getElementById(\"myLinks\");
+      var y = document.getElementById(\"client_name\"); 
+      var z = document.getElementById(\"tlu_logo\");
+      
+      if (x.style.display === \"block\") {
+        x.style.display = \"none\";
+        y.style.display = \"flex\";
+        z.style.display = \"flex\";
+      } else {
+        x.style.display = \"block\";
+        y.style.display = \"none\";
+        z.style.display = \"none\";
+      }
+    }
+</script>
+  
+   <form method=\"POST\" action=\"". htmlspecialchars($_SERVER["PHP_SELF"])."\">
         <button type=\"submit\" id=\"logout\" name=\"logout\">
             <img id='logout_img' src=\"../images/logout.png\" alt=\"logout\">
         </button>       
-    </form>
+    </form> 
+  
+   
     <link rel=\"stylesheet\" href=\"../style.css\">
 </div>";
