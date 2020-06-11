3f21ea64cae890055b6410e38b14be0778b7f015
uWookies <martten@tlu.ee>
2020-06-11 12:35:42 +0300

    desktop main menu update, fixed edge support

--- kalender/index.php
+++ kalender/index.php
@@ -120,14 +120,12 @@ if(!isset($_SESSION["id"])){
 </head>
 <body>
 <?php require('../nav-bar.php'); ?>
-<div class="links">
     <div class="links">
         <a href="../statistika/" class="page" ><span class="link_names">Statistika</span><img src="../images/statistics.png" alt="statistics" class="link_icons" id="first_icon"></a>
         <a href="../aine/" class="page"> <span class="link_names">Aine</span><img src="../images/add.png" alt="statistics" class="link_icons" id="second_icon"></a>
         <a href="../kalender/" class="page" id="chosen"><span class="link_names">Kalender</span><img src="../images/calendar.png" alt="statistics" class="link_icons" id="third_icon"></a>
         <a href="../seaded/" class="page" id="tools"><span class="link_names">Ajalugu</span><img src="../images/history.png" alt="statistics" class="link_icons" id="fourth_icon"></a>
     </div>
-</div>
 
 <div id="popup" class="popup">
     <span class="helper"></span>
