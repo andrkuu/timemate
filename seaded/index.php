8cca1c52ef615becd5af388152aa3b2ac17e013f
uWookies <martten@tlu.ee>
2020-06-11 11:44:17 +0300

    Main menu design change and restructured

--- seaded/index.php
+++ seaded/index.php
@@ -18,7 +18,7 @@ if(!isset($_SESSION["id"])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="../style.css">
       <link rel="stylesheet" href="tools.css">
-    <title>Seaded</title>
+    <title>Ajalugu</title>
       <script src="../jquery.js"></script>
     <script>
 
@@ -76,18 +76,12 @@ if(!isset($_SESSION["id"])){
   <body>
     <?php include('../nav-bar.php'); ?>
     <div class="links">
-        <a href="../statistika/" class="page" id="tools"><span class="link_names">Statistika</span></a>
-        <a href="../aine/" class="page" id="tools"> <span class="link_names">Aine</span></a>
-        <a href="../kalender/" class="page" id="tools"><span class="link_names">Kalender</span></a>
-        <a href="../seaded/" class="page" id="chosen" id="tools"<span class="link_names">Ajalugu</span></a>
+        <a href="../statistika/" class="page" id="tools"><span class="link_names">Statistika</span><img src="../images/statistics.png" alt="statistics" class="link_icons" id="first_icon"></a>
+        <a href="../aine/" class="page" id="tools"> <span class="link_names">Aine</span><img src="../images/add.png" alt="statistics" class="link_icons" id="second_icon"></a>
+        <a href="../kalender/" class="page" id="tools"><span class="link_names">Kalender</span><img src="../images/calendar.png" alt="statistics" class="link_icons" id="third_icon"></a>
+        <a href="../seaded/" class="page" id="chosen" id="tools"<span class="link_names">Ajalugu</span><img src="../images/history.png" alt="statistics" class="link_icons" id="fourth_icon"></a>
     </div>
 
-    <div class="images">
-    <img src="../images/statistics.png" alt="statistics" class="link_icons" id="first_icon">
-    <img src="../images/add.png" alt="statistics" class="link_icons" id="second_icon">
-    <img src="../images/calendar.png" alt="statistics" class="link_icons" id="third_icon">
-    <img src="../images/history.png" alt="statistics" class="link_icons" id="fourth_icon">
-    </div>
 
         <div id="history">
             <div id="history_header">Sinu eelmised sisestused</div>
